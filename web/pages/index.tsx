964b37557d3640db83933322dfa9b68f4e966d99
willgardner9 <willgardner9@gmail.com>
2022-02-04 15:37:26 +0000

    feat: landing page pricing section

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -18,19 +18,23 @@ import Wordpress from "../components/WorksWith/Wordpress";
 import {IoIosInfinite} from "react-icons/io";
 import {CgBatteryFull} from "react-icons/cg";
 import {RiLock2Line} from "react-icons/ri";
-import {BsLightningCharge} from "react-icons/bs";
+import {BsCheck, BsLightningCharge} from "react-icons/bs";
 import {AiOutlineFieldTime} from "react-icons/ai";
 import {HiOutlineCog} from "react-icons/hi";
 import {RiDashboard3Line} from "react-icons/ri";
+import {useState} from "react";
+import {Switch} from "@headlessui/react";
 
 const Home: NextPage = () => {
+  const [enabled, setEnabled] = useState(false);
+
   const features = [
     {
       name: "Unlimited tests & domains",
       description:
         "Set up as many A/B tests as you like on as many domains as you own. Perfect for indiehackers with multiple projects, freelancers, and agencies.",
       icon: (
-        <IoIosInfinite className="w-10 h-10 p-2 rounded-lg bg-green-500 text-white" />
+        <IoIosInfinite className="w-10 h-10 p-2 rounded-lg bg-green-600 text-white" />
       ),
     },
     {
@@ -38,7 +42,7 @@ const Home: NextPage = () => {
       description:
         "A/B test copy, images, videos, and element visibility. Perfect for tring out different hero copy, CTAs, and element orders.",
       icon: (
-        <CgBatteryFull className="w-10 h-10 p-2 rounded-lg bg-green-500 text-white" />
+        <CgBatteryFull className="w-10 h-10 p-2 rounded-lg bg-green-600 text-white" />
       ),
     },
     {
@@ -46,7 +50,7 @@ const Home: NextPage = () => {
       description:
         "All data is secured safely in EU servers. We don't track your users or store their personal data—and you own all your data.",
       icon: (
-        <RiLock2Line className="w-10 h-10 p-2 rounded-lg bg-green-500 text-white" />
+        <RiLock2Line className="w-10 h-10 p-2 rounded-lg bg-green-600 text-white" />
       ),
     },
     {
@@ -59,15 +63,7 @@ const Home: NextPage = () => {
         </>
       ),
       icon: (
-        <RiDashboard3Line className="w-10 h-10 p-2 rounded-lg bg-green-500 text-white" />
-      ),
-    },
-    {
-      name: "Realtime data",
-      description:
-        "View all your A/B test data in realtime. No waiting until the end of the day/week for reports and rollups.",
-      icon: (
-        <AiOutlineFieldTime className="w-10 h-10 p-2 rounded-lg bg-green-500 text-white" />
+        <RiDashboard3Line className="w-10 h-10 p-2 rounded-lg bg-green-600 text-white" />
       ),
     },
     {
@@ -75,7 +71,7 @@ const Home: NextPage = () => {
       description:
         "Our JavaScript snippet runs in the browser, so say goodbye to content flickering.",
       icon: (
-        <BsLightningCharge className="w-10 h-10 p-2 rounded-lg bg-green-500 text-white" />
+        <BsLightningCharge className="w-10 h-10 p-2 rounded-lg bg-green-600 text-white" />
       ),
     },
     {
@@ -87,9 +83,34 @@ const Home: NextPage = () => {
         </>
       ),
       icon: (
-        <HiOutlineCog className="w-10 h-10 p-2 rounded-lg bg-green-500 text-white" />
+        <HiOutlineCog className="w-10 h-10 p-2 rounded-lg bg-green-600 text-white" />
       ),
     },
+  ];
+
+  const pricing = [
+    {
+      monthly: "£7",
+      annual: "£70",
+      sessions: "10,000",
+    },
+    {
+      monthly: "£12",
+      annual: "£120",
+      sessions: "50,000",
+    },
+    {
+      monthly: "£15",
+      annual: "£150",
+      sessions: "100,000",
+    },
+  ];
+
+  const plansInclude = [
+    "All features",
+    "Unlimited A/B tests",
+    "Unlimited websites & domains",
+    "100% data ownership",
   ];
 
   return (
@@ -293,15 +314,19 @@ const Home: NextPage = () => {
           </div>
         </div>
       </section>
-      <section className="w-screen p-4 lg:px-0 mt-16 bg-slate-50">
+
+      <section className="w-screen p-4 lg:px-0 mt-12">
         <div className="w-full mx-auto max-w-8xl flex flex-col">
           <h2 className="text-slate-900 font-semibold text-5xl text-center leading-tight my-16">
             Powerful features available on all plans
           </h2>
-          <div className="w-full flex flex-wrap justify-evenly">
+          <div className="w-full flex flex-wrap justify-evenly gap-4">
             {features.map((feature) => {
               return (
-                <article className="flex flex-col md:basis-1/3 lg:basis-1/4 px-6 mb-12">
+                <article
+                  className="flex flex-col rounded-lg w-80 p-8 "
+                  key={feature.name}
+                >
                   {feature.icon}
                   <h4 className="text-slate-900 font-medium text-xl mt-2 leading-10">
                     {feature.name}
@@ -315,12 +340,97 @@ const Home: NextPage = () => {
           </div>
         </div>
       </section>
-      <section className="w-screen p-4 lg:px-0 mt-12">
+      <section className="w-screen p-4 lg:px-0 mt-12 pb-10">
+        <div className="w-full mx-auto max-w-8xl flex items-center flex-col">
+          <h2 className="text-slate-900 font-semibold text-5xl text-center leading-tight mt-8">
+            Transparent pricing
+          </h2>
+          <Switch.Group>
+            <div className="flex items-center mt-12 mb-8 md:mb-0">
+              <Switch.Label className="mr-4 text-slate-700 max-w-3xl font-light text-xl">
+                Monthly billing
+              </Switch.Label>
+              <Switch
+                checked={enabled}
+                onChange={setEnabled}
+                className={`${
+                  enabled ? "bg-green-500" : "bg-slate-300"
+                } relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500`}
+              >
+                <span
+                  className={`${
+                    enabled ? "translate-x-6" : "translate-x-1"
+                  } inline-block w-4 h-4 transform bg-white rounded-full transition-transform`}
+                />
+              </Switch>
+              <Switch.Label className="ml-4 text-slate-700 max-w-3xl font-light text-xl relative flex items-center">
+                Yearly billing{" "}
+                <span className="bg-orange-100 py-1 px-2 rounded-full text-orange-600 font-medium text-xs absolute w-fit md:-right-28 -bottom-8 md:-bottom-0">
+                  2 months free
+                </span>
+              </Switch.Label>
+            </div>
+          </Switch.Group>
+          <div className="w-full flex flex-col sm:flex-row justify-center gap-10 my-12">
+            <div className="flex w-full sm:w-1/4 flex-col gap-4">
+              {pricing.map((price) => {
+                return (
+                  <article
+                    className="flex flex-col rounded-lg border pl-4 py-4"
+                    key={price.monthly}
+                  >
+                    <h4 className="text-slate-900 font-medium text-xl leading-10">
+                      {enabled
+                        ? price.annual + " / year"
+                        : price.monthly + " / month"}
+                    </h4>
+                    <p className="text-slate-500 leading-8">
+                      {price.sessions} sessions / month
+                    </p>
+                  </article>
+                );
+              })}
+            </div>
+            <div className="flex flex-col w-full items-center sm:items-start sm:w-1/4 bg-white">
+              <h4 className="text-slate-900 font-medium text-xl leading-10">
+                All plans include
+              </h4>
+              <ul>
+                {plansInclude.map((feature) => {
+                  return (
+                    <li className="flex items-center" key={feature}>
+                      <span>
+                        <BsCheck className="text-green-600" />
+                      </span>
+                      <span className="ml-2 text-slate-500 leading-8 font-light">
+                        {feature}
+                      </span>
+                    </li>
+                  );
+                })}
+              </ul>
+              <Link href="/auth/sign-up" passHref>
+                <a className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700 transition-all z-10 mt-4 w-fit">
+                  Get started with a free trial
+                </a>
+              </Link>
+              <a
+                className="mt-6 underline text-xs font-light text-slate-500 w-fit"
+                href=""
+              >
+                Have more traffic? Contact us
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="w-screen p-4 lg:px-0 py-12 bg-slate-900 relative">
+        <div className="footer-gradient absolute w-full"></div>
         <div className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center">
-          <h2 className="text-slate-900 font-semibold text-4xl text-center leading-tight z-10">
+          <h2 className="text-slate-100 font-semibold text-4xl text-center leading-tight z-10">
             Ready to start A/B testing?
           </h2>
-          <h3 className="text-slate-700 max-w-3xl font-light text-xl text-center mt-4 z-10">
+          <h3 className="text-slate-300 max-w-3xl font-light text-xl text-center mt-4 z-10">
             A/B testing shouldn't be so difficult and time-consuming.{" "}
             <span className="font-oswald">TESTA/BLY</span> is a no-code A/B
             testing tool that makes optimizing your conversion rate quick and
