abb26b05f2893aa801b4a1abe8491fb07e2b3db4
willgardner9 <willgardner9@gmail.com>
2022-03-31 21:36:47 +0100

    feat: embed script changes to session/conversion counting

--- embed-script/testably.babel.js
+++ embed-script/testably.babel.js
@@ -1,27 +1,21 @@
 "use strict";
-
 (function () {
   var _cookies$find2;
-
   const _cookies = (document.cookie && document.cookie.split("; ")) || false;
-
+  const _api = "http://127.0.0.1:3333/api/v1/";
   const _userId = "bcff9d7b-886a-43fd-92a4-41b2db0ede63";
   const _testId = "b97001dd-89c8-41e4-b575-cdebdd05e318";
   const _testPageURL = "http://127.0.0.1:5500/embed-script/demo/index.html";
   const _conversionURL = "http://127.0.0.1:5500/embed-script/demo/pricing.html";
   const _selector = "h2";
-
   const _dom = document.querySelector(_selector);
-
   const _type = "copy";
   const _variations = {
     "f1ff73ae-149f-43a3-8bf8-c5dc73ae5df4": "Privacy-friendly A/B testing",
     "86a92a88-4d61-42e9-a460-89c22daecf54": "No-code A/B testing",
   };
-
   function _getActiveVariationId() {
     var _cookies$find;
-
     let activeVariationId =
       (_cookies &&
         ((_cookies$find = _cookies.find((row) =>
@@ -30,7 +24,6 @@
           ? void 0
           : _cookies$find.split("=")[1])) ||
       false;
-
     if (!activeVariationId || !_variations[activeVariationId]) {
       activeVariationId =
         Object.keys(_variations)[
@@ -39,14 +32,10 @@
       document.cookie =
         "testably_variation=" + activeVariationId + ";max-age=2592000";
     }
-
     return activeVariationId;
   }
-
   const _activeVariationId = _getActiveVariationId();
-
   const _url = window.location.href;
-
   const _converted =
     (_cookies &&
       ((_cookies$find2 = _cookies.find((row) =>
@@ -55,9 +44,7 @@
         ? void 0
         : _cookies$find2.split("=")[1])) ||
     false;
-
   if (_url !== _testPageURL && _url !== _conversionURL) return;
-
   if (_url === _testPageURL) {
     switch (_type) {
       case "copy":
@@ -66,25 +53,21 @@
           _dom.value = _variations[_activeVariationId];
         }
         break;
-
       case "src":
         {
           _dom.src = _variations[_activeVariationId];
         }
         break;
-
       case "visibility":
         {
           if (_variations[_activeVariationId] === "hidden")
             _dom.style.display = "none";
         }
         break;
-
       default:
         break;
     }
-
-    fetch("http://127.0.0.1:3333/api/v1/sessions", {
+    fetch(_api + "sessions", {
       method: "post",
       mode: "cors",
       headers: {
@@ -97,16 +80,13 @@
       }),
     });
   }
-
   function _testablyConversion() {
     if (_converted) return;
     document.cookie = "testably_converted=true;max-age=2592000";
-    fetch("http://127.0.0.1:3333/api/v1/conversions", {
+    fetch(_api + "conversions", {
       method: "post",
       mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: {"Content-Type": "application/json"},
       body: JSON.stringify({
         userId: _userId,
         testId: _testId,
@@ -114,14 +94,11 @@
       }),
     });
   }
-
   if (_url === _conversionURL && _activeVariationId) {
     _testablyConversion();
   }
-
   window.addEventListener("popstate", function (e) {
     console.log("detected url change to:", window.location.href);
-
     if (_url === _conversionURL && _activeVariationId) {
       _testablyConversion;
     }
